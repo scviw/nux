6fb7c35ec8e1924bf6206131d799d02137019a27
P231014735 马英杰 <1570326863@qq.com>
2025-07-08 00:42:33 +0800

    Add files via upload

---
+++ src_app_practice_06-news02_page_tsx_8450df25._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_6bab6250._.js",
+  "static/chunks/src_app_practice_06-news02_page_tsx_fb80c5ba._.js"
+],
+    source: "dynamic"
+});
---
+++ src_app_practice_embed-demo_page_tsx_8450df25._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_9d96a264._.js"
+],
+    source: "dynamic"
+});
